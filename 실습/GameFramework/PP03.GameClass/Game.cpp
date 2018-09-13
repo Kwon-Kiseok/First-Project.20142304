8f3c8770983900277db1c0e84ac13e8e63fcd702
user <user@532-41>
2018-09-13 18:10:04 +0900

    9월 13일 PP04.BasicDrawing

--- 실습/GameFramework/PP03.GameClass/Game.cpp
+++ 실습/GameFramework/PP03.GameClass/Game.cpp
@@ -3,6 +3,10 @@
 bool Game::init(const char*title, int xpos, int ypos,
 	int width, int height, bool fullscreen)
 {
+	//main.cpp에서 m_bRunning 으로 게임 반복문의 조건을 보고 있음
+	//Game.h 에서 m_bRunning 이 false 값으로 초기화 되어있기 때문에 
+	//init이 될 때 게임이 돌아간다는 것을 보여주기 위해 true로 할당해줌
+	m_bRunning = true;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
@@ -10,9 +14,19 @@ bool Game::init(const char*title, int xpos, int ypos,
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
 			640, 480, SDL_WINDOW_SHOWN);
-
+		
 		if (m_pWindow != 0)
+		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+		}
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+
+		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
 	}
 	else
 	{
@@ -22,9 +36,17 @@ bool Game::init(const char*title, int xpos, int ypos,
 }
 
 void Game::render()
-{
-	SDL_RenderClear(m_pRenderer); //draw color로 render 지우기
-	SDL_RenderPresent(m_pRenderer); //화면 제시하기
+{	
+	//원본 사각형 : m_sourceRectangle    대상 사각형 : m_destinationRectangle
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w; 
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
+	SDL_RenderClear(m_pRenderer);
+	//원본 사각형과 대상 사각형의 위치와 크기에 따라 화면에 다르게 나타남
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderPresent(m_pRenderer);
 }
 
 void Game::clean()
